2ddbcedc0feb100082779fdc2cd9a3363b8ebd2a
Hoanhuy2003 <hoan455554@gmail.com>
2025-12-04 15:29:51 +0700

    fix thietBiRepo

--- project_one/src/main/java/com/thiet_thi/project_one/repositorys/ThietBiRepository.java
+++ project_one/src/main/java/com/thiet_thi/project_one/repositorys/ThietBiRepository.java
@@ -2,8 +2,12 @@ package com.thiet_thi.project_one.repositorys;
 
 import com.thiet_thi.project_one.models.ThietBi;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ThietBiRepository extends JpaRepository<ThietBi, String> {
 
-    long countByMaDonVi(String maDonVi);
+    // ThietBiRepository.java – SỬA THÀNH CÁI NÀY LÀ XONG!
+    @Query("SELECT COUNT(tb) FROM ThietBi tb WHERE tb.phong.donVi.maDonVi = :maDonVi")
+    long countByDonVi(@Param("maDonVi") String maDonVi);
 }
